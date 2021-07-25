15fe0285765366a4bb92e0f6b02ede084d04b2ae
Wahyu Kristianto <w.kristories@gmail.com>
2021-07-26 03:11:27 +0700

    Update QuranServiceProvider.php

--- src/QuranServiceProvider.php
+++ src/QuranServiceProvider.php
@@ -6,13 +6,5 @@ use Illuminate\Support\ServiceProvider;
 
 class QuranServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any package services.
-     *
-     * @return void
-     */
-    public function register(): void
-    {
-        $this->mergeConfigFrom(__DIR__ . '/../config/quran.php', 'quran');
-    }
+    //
 }
